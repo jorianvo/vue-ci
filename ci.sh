a0082d40bec4b50fda7fdd13fa23e74c8a17797b
Jorian van Oostenbrugge <jovofdt@gmail.com>
2019-08-18 21:08:59 +0200

    Refactor run function to test if we are running locally to create the proper docker tag

--- ci.sh
+++ ci.sh
@@ -2,17 +2,25 @@
 readonly _IMAGE="jorianvo/vue-ci"
 
 function _run () {
+    # Locally we don't have node installed (or access to travis the build env variables)
+    # so we just build the image using the latest tag
+    if command -v node >/dev/null 2>&1; then
+        local _VUE_VERSION=$(node vueVersion.js)
+        local _TAG_MAJOR_PATCH=$(node vueVersion.js --short)
+        local _TAG="${_VUE_VERSION}-b${TRAVIS_BUILD_NUMBER}"
+    else
+        local _TAG="latest"
+    fi
     docker run -v "$PWD:/site" -w "/site" "$_IMAGE:${_TAG}" $1
 }
 
 function build () {
-    local _VUE_VERSION=$(node vueVersion.js)
-    local _TAG_MAJOR_PATCH=$(node vueVersion.js --short)
-    
     # Locally we don't have node installed (or access to travis the build env variables)
     # so we just build the image using the latest tag
     if command -v node >/dev/null 2>&1; then
-        _TAG="${_VUE_VERSION}-b${TRAVIS_BUILD_NUMBER}"
+        local _VUE_VERSION=$(node vueVersion.js)
+        local _TAG_MAJOR_PATCH=$(node vueVersion.js --short)
+        local _TAG="${_VUE_VERSION}-b${TRAVIS_BUILD_NUMBER}"
         docker build -t "$_IMAGE:${_TAG}" -t "$_IMAGE:${_TAG_MAJOR_PATCH}" .
     else
         docker build -t "$_IMAGE:latest" .
